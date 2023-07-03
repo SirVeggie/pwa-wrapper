72c268ac47b161df9a5ba1439ebf53704d6760ec
Veikka <32365239+SirVeggie@users.noreply.github.com>
2023-07-03 23:19:36 +0300

    prevent refresh

--- src/routes/[url]/+page.svelte
+++ src/routes/[url]/+page.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
     import { page } from "$app/stores";
     import Wrapper from "$lib/components/wrapper.svelte";
+    import { onMount } from "svelte";
 
     $: url = decodeURIComponent($page.params.url ?? "");
+
+    onMount(() => {
+        window.addEventListener("beforeunload", preventRefresh);
+        return () => {
+            window.removeEventListener("beforeunload", preventRefresh);
+        };
+    });
+
+    function preventRefresh(e: BeforeUnloadEvent) {
+        e.preventDefault();
+        e.returnValue = "";
+    }
 </script>
 
 <div>
