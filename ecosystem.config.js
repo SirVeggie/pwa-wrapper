f011e44359f0393844ec8aaf5fced815bcad76ed
Veikka <32365239+SirVeggie@users.noreply.github.com>
2023-07-03 18:58:29 +0300

    asd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   apps: [{
     name: 'wrapper',
-    script: 'node build',
-    // script: 'node -r dotenv/config build',
-    // script: 'build/index.js',
+    script: 'build/index.js'
   }]
 };
